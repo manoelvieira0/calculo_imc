34df19129e136fcb10bdc2928adb8c901255dc39
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2021-12-21 13:49:45 -0300

    Calculo Imc

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String infoText = 'Informe seus dados';
+
+  void resetFields() {
+    weightController.text = '';
+    heightController.text = '';
+    infoText = 'Informe seus dados';
+  }
+
+  void calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        infoText = 'Abaixo do Peso (${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        infoText = "Levemente Acima do Peso(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        infoText = "Obesidade Grau I(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        infoText = "Obesidade Grau II(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        infoText = "Obesidade Grau III(${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +59,9 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green[500],
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              resetFields();
+            },
             icon: Icon(Icons.refresh),
           )
         ],
@@ -50,6 +85,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 16.0),
+              controller: weightController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -59,13 +95,14 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 16.0),
+              controller: heightController,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: Container(
                 height: 40,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: calculate,
                   child: Text(
                     'Calcular',
                     style: TextStyle(fontSize: 16),
@@ -74,7 +111,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              'Info',
+              infoText,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 16),
             )
